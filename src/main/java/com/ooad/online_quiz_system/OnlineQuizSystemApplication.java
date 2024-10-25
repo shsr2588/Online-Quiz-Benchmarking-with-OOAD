994dcba81b0e0a190fac4589556625f52e847d07
Shreshth Srivastava <shsr2588@colorado.edu>
2024-10-25 09:18:19 -0600

    Setup project

---
+++ src/main/java/com/ooad/online_quiz_system/OnlineQuizSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ooad.online_quiz_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooad/online_quiz_system/OnlineQuizSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooad.online_quiz_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
